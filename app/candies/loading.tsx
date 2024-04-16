e4fee328ae763faef13d8386b72432e653862bc1
Akarikev <princeelorm17@gmail.com>
2024-04-16 22:24:23 +0100

    mod: loader animation

--- app/candies/loading.tsx
+++ app/candies/loading.tsx
@@ -1,7 +1,12 @@
+import { Loader } from "lucide-react";
 import React from "react";
 
 function loading() {
-  return <div>loading</div>;
+  return (
+    <div className="flex flex-col justify-center items-center ">
+      <Loader className="animate-spin w-7 h-7" />
+    </div>
+  );
 }
 
 export default loading;
